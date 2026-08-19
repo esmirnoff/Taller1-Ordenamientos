adca62f4fa6bc4d1654a3ac6eb2725258188f6b3
esmirnoff <sromeroc1907@gmail.com>
2026-08-19 02:21:22 -0500

    Update MisAlgoritmos.java

--- martinelimpar/src/MisAlgoritmos.java
+++ martinelimpar/src/MisAlgoritmos.java
@@ -2,12 +2,21 @@ public class MisAlgoritmos implements Busquedas, Ordenamientos{
 
  @Override
  public int linearSearch(int[] arrayPorExplorar, int elementoABuscar) {
-  for(int i = 0; i < arrayPorExplorar.length; i++){;
+  for(int i = 0; i < arrayPorExplorar.length; i++){
  if(arrayPorExplorar[i]==elementoABuscar){return i;
  }
-  return -1;}
+  }return -1;
  }
-
+public static void main(String[] args) {
+  int[] numeros = {10, 20, 30, 40, 50};
+  MisAlgoritmos alg= new MisAlgoritmos();
+  int search1 = 30;
+  int result1 = alg.linearSearch(numeros, search1);
+  System.out.println("Buscando "+search1+ ": indice = "+result1);
+  int search2 = 99;
+  int result2 = alg.linearSearch(numeros, search2);
+  System.out.println("Buscando "+search2+ ": indice = "+result2);
+}
  @Override
  public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
   return 0;
